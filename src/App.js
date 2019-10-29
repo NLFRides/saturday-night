9dec9ffca85f37ee5036db39a640fcbed8484cd3
Jason <jhwang73>
2019-10-28 21:28:20 -0500

    Setup left right wrappers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import SplitPane from "react-split-pane";
+import RightWrapper from "./components/rightWrapper"
+import LeftWrapper from "./components/leftWrapper"
 
 function App() {
   return (
     <div className="Resizer">
-      <SplitPane split="vertical" minSize={50} defaultSize={1000}>
-        <div>Left Pane</div>
-        <div>Right Pane</div>
+      <SplitPane split="vertical" minSize={50} defaultSize={500}>
+        <RightWrapper/>
+        <LeftWrapper/>
       </SplitPane>
     </div>
   );
