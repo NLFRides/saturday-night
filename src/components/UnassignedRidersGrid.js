aab284022348c03b543f424bab376e84f2826384
Jason <jhwang73>
2020-01-30 10:26:01 -0600

    Testing grid

--- src/components/UnassignedRidersGrid.js
+++ src/components/UnassignedRidersGrid.js
@@ -5,8 +5,26 @@ import List from '@material-ui/core/List';
 import { UNASSIGNED_RIDERS_GRID_ID, RIDER_TYPE } from "../constants";
 import { useSelector } from "react-redux";
 import { Droppable } from "react-beautiful-dnd";
-
 import Rider from "./Rider";
+
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
+
 
 const styles = {
   unassignedRidersGrid: {
@@ -16,29 +34,36 @@ const styles = {
     maxHeight: 150,
     overflow: 'auto',
     columns: "2",
-    // display: "flex",
-    // justifyContent: "space-between",
   }
 }
 
+
 const UnassignedRidersGrid = () => {
+
+  const classes = useStyles();
 
   const unassignedRiders = useSelector(state => state.unassignedRiders);
 
   return (
-    <Droppable droppableId={UNASSIGNED_RIDERS_GRID_ID} direction="vertical" type={RIDER_TYPE}>
+    <Droppable droppableId={UNASSIGNED_RIDERS_GRID_ID} direction="horizontal" type={RIDER_TYPE}>
       {provided => (
-        <div {...provided.droppableProps} ref={provided.innerRef} >
-          <Paper style={styles.unassignedRidersGrid} variant='outlined' >
-            <List>
+        <div {...provided.droppableProps} ref={provided.innerRef} className={classes.root} >
+          <Grid container spacing={3}>
+            {/* <List> */}
               { unassignedRiders.map((riderId, index) => 
+                <Grid item xs>
+                  
+                
                 <Rider 
                   key={riderId} 
                   index={index}
-                  riderId={riderId} />)
+                  riderId={riderId} />
+                  </Grid>
+                  
+                  )
               }
-            </List>
-          </Paper>
+            {/* </List> */}
+          </Grid>
           {/* {provided.placeholder} */}
         </div>
       )}
